ea12fbc7df6c63c22caa1222386af82808323b96
d-rezzer <d-rezzer@pop-os.localdomain>
2019-05-24 15:27:17 -0600

    basic path parsing finished

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,11 @@ use std::io::Cursor;
 use ftp::FtpStream;
 use std::net::{SocketAddr, ToSocketAddrs};
 use std::iter::Iterator;
+
 fn main() {
+
+    let sex_offender_path  = "/state/sex_offender";
+
     let mut ftp_stream = FtpStream::connect("ftptds.shadowsoft.com:21")
         .unwrap_or_else(|err| {
             panic!("{}", err);
@@ -14,13 +18,7 @@ fn main() {
 
     println!("Current directory {}", ftp_stream.pwd().unwrap());
 
-    //retrieve a file from server.
-    //let ll = ftp_stream.cwd("test_data").unwrap();
 
-    struct FileInfo {
-        name: String,
-        last_mod: String,
-    }
 
     struct SexOffenderFileInfo {
         image: String,
@@ -28,37 +26,71 @@ fn main() {
 
     }
 
-    //let lst = ftp_stream.list(Some("us")).unwrap(); //list of state paths.
     let lst = ftp_stream.nlst(Some("us")).unwrap();
-    let tt: Vec<(String, String)>  = vec![("this".to_string(),"that".to_string())];
 
-    let available_states: Vec<String> = lst.into_iter().map(| p | {
+    #[derive(Debug)]
+    struct FileInfoS(pub String, pub String, pub String, pub String);
+
+
+    let available_states: Vec<FileInfo> = lst.into_iter().map(| p | {
+
         let mut pp = p.to_string();
-        pp.push_str("/state/sex_offender");
+        pp.push_str(sex_offender_path);
 
+        //list the files available for this state
+        //map_err?
+       let res: Vec<FileInfo> = ftp_stream.list(Some(&pp))
+           .into_iter()
+           .flatten()
+           .filter(|fi| fi.contains("records") || fi.contains("images"))
+           .map(|fi| {
 
-       let res: Vec<String> = ftp_stream.list(Some(&pp)).into_iter()
-           .map(|item| {
-               item
-        }).flatten()
-          .inspect(|item| {
+               create_file_info(&p, &fi)
 
-            println!("current item {}:: {:?}", &p, item);
-        })
+           })
            .collect();
 
-        "Ssssuper!".to_string()
+       res
 
-    }).collect();
+    }).flatten().collect();
+
 
     for file in available_states {
         println!("{:?}", file);
     }
-   // println!("{:?}", lst);
 
     println!("somthings happening here, what it is ain't exactly clear");
 
     let _   = ftp_stream.quit();
+
+}
+
+#[derive(Debug)]
+    struct FileInfo {
+
+        path: Option<String>,
+
+        name: Option<String>,
+        year: Option<String>,
+        month: Option<String>,
+        day: Option<String>,
+        size: Option<String>, //convert this to i64
+
+    }
+
+fn create_file_info(path: &str, line: &str) -> FileInfo {
+
+        let mut iter = line.split_whitespace().rev().take(5);
+
+        FileInfo {
+            path: Some(path.to_string()),
+            name : Some(iter.next().unwrap().to_string()),
+            year : Some(iter.next().unwrap().to_string()),
+            month : Some(iter.next().unwrap().to_string()),
+            day : Some(iter.next().unwrap().to_string()),
+            size : Some(iter.next().unwrap().to_string()),
+        }
+
 
 }
 
