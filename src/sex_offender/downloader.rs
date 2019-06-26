4a2c8ae059c5a136ba2c43edb6e6606cb6bb68dc
Ryan Lee Martin <ryanlmartin@gmail.com>
2019-06-25 18:29:41 -0600

    wrote available_updates function

--- src/sex_offender/downloader.rs
+++ src/sex_offender/downloader.rs
@@ -8,19 +8,28 @@ use std::path::{self, Path, PathBuf};
 use std::str;
 use std::io::{BufWriter, Write, Read, ErrorKind, BufReader, copy};
 use std::convert::{AsRef, From};
-use core::borrow::Borrow;
+use core::borrow::{Borrow, BorrowMut};
 use std::process::id;
 use std::{self, error::Error};
 use zip;
 use zip::ZipArchive;
 use serde_derive::{Serialize, Deserialize};
 use serde_json;
+use rusqlite::{self, Connection, NO_PARAMS};
+use serde_rusqlite;
+use serde_rusqlite::from_row;
+use std::collections::HashSet;
+use std::clone::Clone;
+
+//TODO: get path info from ENV vars instead of hardcoding.
 static SEX_OFFENDER_PATH: &'static str = "/state/sex_offender";
 static LOCAL_PATH: &'static str = "/home/d-rezzer/dev/eyemetric/ftp";
-
+static IMPORT_LOG: &'static str = "/home/d-rezzer/dev/eyemetric/ftp/importlog.sqlite";
 const CHUNK_SIZE: usize = 2048;
+type GenError = Box<std::error::Error>;
+//type GenResult<T> = Result<T, GenError>;
 
-//pub type Result<T> = ::std::result::Result<T, Box<std::error::Error>>;
+pub type Result<T> = ::std::result::Result<T, Box<std::error::Error>>;
 
 pub enum SexOffenderImportError {
     ConnectionError(std::io::Error),
@@ -40,7 +49,7 @@ pub enum DownloadOption {
 }
 
 impl Downloader {
-    pub fn connect(addr: &str, user: &str, pwd: &str) -> Result<Self, Box<Error>> {
+    pub fn connect(addr: &str, user: &str, pwd: &str) -> Result<Self> {
         //these values, I assume will be a configuration.
 
         let mut sex_offender_importer = Downloader {
@@ -59,8 +68,12 @@ impl Downloader {
     }
 
 
-    fn create_file_info(&self, remote_file_path: &str, ftp_line: &str) -> Result<FileInfo, Box<Error>> {
+    fn create_file_info(&self, remote_file_path: &str, ftp_line: &str) -> Result<FileInfo> {
         let mut line = ftp_line.split_whitespace();
+        //not sure about this.
+        if line.clone().count() == 0 {
+            return Err(GenError::from(format!("Unable to create FileInfo for {}", remote_file_path)));
+        }
         let size = line.nth(4).unwrap();
         let name = line.last().unwrap();
         let nsplit: Vec<&str> = name.split("_").collect();
@@ -69,14 +82,12 @@ impl Downloader {
         let m = nsplit.get(2).unwrap();
         let d = nsplit.get(3).unwrap();
 
-
         let fin = FileInfo::Record(RecordInfo {
             rpath: Some(remote_file_path.to_string()),
             name: Some(name.to_string()),
-            year: Some(y.to_string()),
-            month: Some(m.to_string()),
-            day: Some(d.to_string()),
+            last_modified: Some(format!("{}-{}-{}", y, m, d)),
             size: Some(size.to_string()),
+            status: RecordStatus::None, //None is the beginning state.
         });
 
         println!("{:?}", &fin);
@@ -86,7 +97,7 @@ impl Downloader {
 
     ///returns a list of available files for download from remote server.
     ///a filter can be passed in to narrow the list.
-    pub fn remote_file_list(&mut self, filter: fn(&String) -> bool, file_opt: DownloadOption) -> Vec<Result<FileInfo, Box<Error>>> {
+    pub fn remote_file_list(&mut self, filter: fn(&String) -> bool, file_opt: DownloadOption) -> Vec<Result<FileInfo>> {
 
         //TODO: This list should be logged and checked against later when filtering on new files.
         //top level state folders (ie us/new_jersey)
@@ -101,13 +112,13 @@ impl Downloader {
 
         let hard_filter = |x: &String| !x.contains(".txt") && !x.contains("united_states");
 
-        let available_files: Vec<Result<FileInfo, Box<Error>>> = state_folders
+        let available_files: Vec<Result<FileInfo>> = state_folders
             .into_iter()
             .map(|state_folder| {
                 let sex_offender_folder = format!("{}{}", state_folder.to_string(), SEX_OFFENDER_PATH);
                 println!("{}", &sex_offender_folder);
 
-                let file_list: Vec<Result<FileInfo, Box<Error>>> = self.stream
+                let file_list: Vec<Result<FileInfo>> = self.stream
                     .list(Some(&sex_offender_folder)) //list remote dir
                     .into_iter()
                     .flatten()
@@ -126,6 +137,29 @@ impl Downloader {
         available_files
     }
 
+    fn available_updates(remote_files: Vec<Result<FileInfo>>) -> Vec<FileInfo> {
+        //get the difference between what we've previously imported and the remote files,
+        //if any
+        let conn = Connection::open(IMPORT_LOG).expect("unable to open a proper db connection");
+        let mut stmt = conn.prepare("SELECT * from remote_file_list").expect("unable to prepare query");
+        let mut import_log = stmt
+            .query_and_then(NO_PARAMS, from_row::<FileInfo>)
+            .expect("Unable to get remote_file_list from Import Log");
+
+        //by_ref vs borrow?
+        if let 0 = import_log.by_ref().count() {
+            return remote_files.into_iter().map(|r| r.unwrap()).collect();
+        }
+        //let cnt = import_log.by_ref().count();
+        //let cnt = import_log.borrow().count();
+        //TODO: this seems bonkers, refactor when you know what you're doing.
+        let h_log: HashSet<FileInfo> = import_log.into_iter().map(|r| r.unwrap()).collect();
+        let h_remote_files: HashSet<FileInfo> = remote_files.into_iter().map(|r| r.unwrap()).collect();
+        let update_diff: Vec<FileInfo> = h_remote_files.difference(&h_log).into_iter().cloned().collect();
+
+        update_diff
+
+    }
     ///returns true if the file on the server is newer than what we have.
     fn file_is_new(fileinfo: &FileInfo) -> bool {
         //new means, remote file is newer or doesn't yet exist on local disk.
@@ -146,7 +180,7 @@ impl Downloader {
         false
     }
     ///downloads the remote archive file and writes to disk.
-    pub fn save_archive(&mut self, fileinfo: &FileInfo) -> Result<SexOffenderArchive, Box<Error>> {
+    pub fn save_archive(&mut self, fileinfo: &FileInfo) -> Result<SexOffenderArchive> {
         let fname = fileinfo.name();//.as_ref().unwrap();
         //make sure we're setup to dload binary files
         self.stream.transfer_type(FileType::Binary)?;
@@ -174,7 +208,7 @@ impl Downloader {
     }
 
     ///write the archive file we got from ftp to disk.
-    fn write_archive(fileinfo: &FileInfo, stream: &mut Read) -> Result<usize, Box<Error>> {
+    fn write_archive(fileinfo: &FileInfo, stream: &mut Read) -> Result<usize> {
         let mut file_path = fileinfo.file_path();
         let mut base_path = fileinfo.base_path();
 
@@ -210,7 +244,7 @@ impl Downloader {
 
     ///extracts an arcive into a list of ExtractedFile types.
     /// An Extracted file can be one of two variants. Csv or ImageArchive
-    pub fn extract_archive(fileinfo: &FileInfo) -> Result<Vec<ExtractedFile>, Box<Error>> {
+    pub fn extract_archive(fileinfo: &FileInfo) -> Result<Vec<ExtractedFile>> {
         let mut extracted_files: Vec<ExtractedFile> = Vec::new(); //store our list of csv files.
         let archive_path = fileinfo.file_path();
         let file = BufReader::new(File::open(archive_path)?);
@@ -254,25 +288,40 @@ pub struct SexOffenderArchive {
 }
 
 
-#[derive(Debug, Serialize,Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Eq, PartialEq, Hash, Clone)]
 pub struct ImageInfo {
     pub rpath: Option<String>,
     pub name: Option<String>,
 }
 
+#[derive(Debug, Serialize, Deserialize, Eq, PartialEq, Hash, Clone)]
+pub struct DownloadInfo {
+    pub started: String,
+    pub bytes_received: i32,
+}
 
-#[derive(Debug, Serialize,Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Eq, PartialEq, Hash, Clone)]
+pub enum RecordStatus {
+    None,
+    InFlight(DownloadInfo),
+    Failed {
+        cause: String,
+        dloadInfo: DownloadInfo,
+    },
+    Success,
+}
+
+#[derive(Debug, Serialize, Deserialize, Eq, PartialEq, Hash, Clone)]
 pub struct RecordInfo {
     pub rpath: Option<String>,
     pub name: Option<String>,
-    pub year: Option<String>,
-    pub month: Option<String>,
-    pub day: Option<String>,
+    pub last_modified: Option<String>,
     pub size: Option<String>, //convert this to i64
+    pub status: RecordStatus,
 }
 
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Eq, PartialEq, Hash, Clone)]
 #[serde(tag = "type")]
 pub enum FileInfo {
     Record(RecordInfo),
